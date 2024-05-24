87c83dfb7769cfb93b11a37981ade606f06e5397
Nathanne <nathanneisip@gmail.com>
2024-05-24 23:01:46 +0800

    Optimized and improved home page view.

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,58 +1,15 @@
 <script setup lang="ts">
-import $ from "jquery";
-import env from "../assets/scripts/config";
 import Navbar from "../components/Navbar.vue";
+import { useRouter } from "vue-router";
 import {
     validateCurrentSession
 } from "@/assets/scripts/session";
 
-validateCurrentSession();
-setInterval(validateCurrentSession, 1000);
-
-setTimeout(()=> {
-    $("#home-route").addClass("selected-menu");
-    $("#mb-home-route").addClass("menu-selected");
-    $("#username").html(localStorage.getItem('username') as string);
-
-    $("#logout-route").click(()=> {
-        $.post(
-            env.host + "/account.php",
-            {
-                action: "logout",
-                hash: localStorage.getItem("hash")
-            },
-            (data)=> {
-                if(data.status == 1) {
-                    localStorage.setItem("username", "null");
-                    localStorage.setItem("theme", "null");
-                    localStorage.setItem("hash", "null");
-                }
-            }
-        );
-    });
-}, 200);
+const router = useRouter();
+validateCurrentSession(router);
+setInterval(()=> validateCurrentSession(router), 1000);
 </script>
 
 <template>
     <Navbar />
 </template>
-
-<style>
-.selected-menu {
-    padding-top: 6px;
-    padding-bottom: 6px;
-    padding-left: 12px;
-    padding-right: 12px;
-    border-radius: 6px;
-    border-color: white;
-    border-style: solid;
-    border-width: 1px;
-}
-
-.menu-selected {
-    padding-bottom: 12px;
-    border-style: solid;
-    border-color: white;
-    border-width: 0px 0px 4px 0px !important;
-}
-</style>
