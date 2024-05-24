bde497b07a89058f61a4e87a2cf6f65547bac1ee
Nathanne <nathanneisip@gmail.com>
2024-05-25 02:00:08 +0800

    Added VCalendar on web app mount.

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,13 @@ import { createApp } from "vue";
 
 import App from "./App.vue";
 import router from "./router";
+import VCalendar from 'v-calendar';
 
+import 'v-calendar/style.css';
 import "../node_modules/bootswatch/dist/lumen/bootstrap.min.css";
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
 
-const app = createApp(App)
-app.use(router)
-app.mount('#app')
+const app = createApp(App);
+app.use(router);
+app.use(VCalendar, {});
+app.mount('#app');
