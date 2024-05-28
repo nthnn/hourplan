584313a7a09cf4dc7b05468dff9794d98d364377
Nathanne <nathanneisip@gmail.com>
2024-05-28 14:46:16 +0800

    Added calendar and to-do list pages on the router.

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
+import Calendar from '@/views/Calendar.vue';
 import HomePage from '@/views/HomePage.vue';
 import Login from '@/views/Login.vue';
 import Signup from '@/views/Signup.vue';
 import SplashScreen from '@/views/SplashScreen.vue';
+import TodoList from '@/views/TodoList.vue';
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -29,6 +31,16 @@ const router = createRouter({
             path: '/home',
             name: 'home',
             component: HomePage
+        },
+        {
+            path: '/todo-list',
+            name: 'todo-list',
+            component: TodoList
+        },
+        {
+            path: '/calendar',
+            name: 'calendar',
+            component: Calendar
         }
     ]
 });
