48eba53270fa0e9a8ccc0c3e78b458034d074355
Nathanne <nathanneisip@gmail.com>
2024-05-28 23:05:48 +0800

    Initial profile page view.

---
+++ src/views/ProfilePage.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import ProfileHeader from "../components/ProfileHeader.vue";
+</script>
+
+<template>
+    <ProfileHeader />
+
+    <br/><br/><br/>
+        <br/><br/><br/>
+        <br/><br/><br/>
+        <br/><br/><br/>
+        <br/><br/><br/>
+        <br/><br/><br/>
+        <br/><br/><br/>
+        <br/><br/><br/>
+        <br/><br/><br/>
+        <br/><br/><br/>
+        <br/><br/><br/>
+        <br/><br/><br/>
+        <br/><br/><br/>
+        <br/><br/><br/>
+        <br/><br/><br/>
+        <br/><br/><br/>
+        <br/><br/><br/>
+        <br/><br/><br/>
+</template>
