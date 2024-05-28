a735172fa2a333f0326bc4ee00b03ea7b2a1b7f2
Nathanne <nathanneisip@gmail.com>
2024-05-28 23:06:04 +0800

    Added paths to router index.ts

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,9 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory, type RouteRecordRaw } from 'vue-router';
 
 import Calendar from '@/views/Calendar.vue';
 import HomePage from '@/views/HomePage.vue';
 import Login from '@/views/Login.vue';
+import ProfilePage from '@/views/ProfilePage.vue';
 import Signup from '@/views/Signup.vue';
 import SplashScreen from '@/views/SplashScreen.vue';
 import TodoList from '@/views/TodoList.vue';
@@ -13,7 +14,8 @@ const router = createRouter({
         {
             path: '/',
             name: 'splash_screen',
-            component: SplashScreen
+            component: SplashScreen,
+            meta: { overflowHidden: true }
         },
         {
             path: '/login',
@@ -41,8 +43,18 @@ const router = createRouter({
             path: '/calendar',
             name: 'calendar',
             component: Calendar
+        },
+        {
+            path: '/profile',
+            name: 'profile',
+            component: ProfilePage
         }
-    ]
+    ] as Array<RouteRecordRaw>,
+    scrollBehavior(_to, _from, savedPosition) {
+        if(savedPosition)
+            return savedPosition;
+        return { top: 0 };
+    }
 });
 
 export default router;
