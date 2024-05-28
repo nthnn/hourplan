a681099952d0be2ff3ab5ed69fcdd3d4c36c370e
Nathanne <nathanneisip@gmail.com>
2024-05-29 01:44:04 +0800

    Function to get user ID using only session hash.

---
+++ backend/lib/session.php
@@ -0,0 +1,20 @@
+<?php
+
+include_once("db_config.php");
+
+function getSessionUserId($hash) {
+    global $db_conn;
+    $res = mysqli_query(
+        $db_conn,
+        "SELECT user_id FROM session WHERE hash=\"".$hash."\""
+    );
+
+    if($res) {
+        $row = mysqli_fetch_row($res);
+        return $row[0];
+    }
+
+    return -1;
+}
+
+?>
