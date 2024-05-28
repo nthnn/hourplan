511c1048a35f5e3dc9864a1f1195912b94e4b6a2
Nathanne <nathanneisip@gmail.com>
2024-05-28 23:04:15 +0800

    Database SQL table for tasks.

--- db.sql
+++ db.sql
@@ -10,3 +10,13 @@ CREATE TABLE session (
   `user_id` int(11) NOT NULL,
   `hash` varchar(255) NOT NULL
 );
+
+CREATE TABLE task (
+  `id` int(11) PRIMARY KEY AUTO_INCREMENT,
+  `desc` varchar(1024) NOT NULL,
+  `start` int(10) NOT NULL,
+  `end` int(10) NOT NULL,
+  `repeat` int(1) NOT NULL,
+  `ends` int(10) NOT NULL,
+  `type` int(1) NOT NULL
+);
