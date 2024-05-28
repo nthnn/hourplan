0e4442ac87d5508436c88e271e1515114e7d0292
Nathanne <nathanneisip@gmail.com>
2024-05-29 02:09:34 +0800

    Added client address and user agent to session table.

--- db.sql
+++ db.sql
@@ -7,18 +7,21 @@ CREATE TABLE users (
 );
 
 CREATE TABLE session (
-  `user_id` int(11) NOT NULL,
-  `hash` varchar(255) NOT NULL
+    `id` int(11) PRIMARY KEY AUTO_INCREMENT,
+    `user_id` int(11) NOT NULL,
+    `hash` varchar(255) NOT NULL,
+    `addr` varchar(255) NOT NULL,
+    `user_agent` varchar(255) NOT NULL
 );
 
 CREATE TABLE task (
-  `id` int(11) PRIMARY KEY AUTO_INCREMENT,
-  `user_id` int(11) NOT NULL,
-  `title` varchar(255) NOT NULL,
-  `desc` varchar(1024) NOT NULL,
-  `start` int(10) NOT NULL,
-  `end` int(10) NOT NULL,
-  `repeat` int(1) NOT NULL,
-  `ends` int(10) NOT NULL,
-  `type` int(1) NOT NULL
+    `id` int(11) PRIMARY KEY AUTO_INCREMENT,
+    `user_id` int(11) NOT NULL,
+    `title` varchar(255) NOT NULL,
+    `desc` varchar(1024) NOT NULL,
+    `start` int(10) NOT NULL,
+    `end` int(10) NOT NULL,
+    `repeat` int(1) NOT NULL,
+    `ends` int(10) NOT NULL,
+    `type` int(1) NOT NULL
 );
