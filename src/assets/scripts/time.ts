d953922d90fc8371f8395ab1419f64534c00d4a2
Nathanne <nathanneisip@gmail.com>
2024-05-29 21:06:14 +0800

    Time function to convert date to short date string.

--- src/assets/scripts/time.ts
+++ src/assets/scripts/time.ts
@@ -3,5 +3,28 @@ export function toUNIX(date: Date): number {
 }
 
 export function toJSDate(unixDate: number): Date {
-    return new Date(unixDate);
+    return new Date(unixDate * 1000);
+}
+
+export function toShortDate(date: Date): string {
+    const monthNames = [
+        "January", "February", "March",
+        "April", "May", "June",
+        "July", "August", "September",
+        "October", "November", "December"
+    ];
+
+    let hours = date.getHours(),
+        minutes = date.getMinutes();
+    const period = hours >= 12 ? 'PM' : 'AM';
+    const paddedMinutes = minutes < 10 ? '0' + minutes : minutes;
+
+    if(hours > 12)
+        hours -= 12;
+    else if(hours === 0)
+        hours = 12;
+
+    return monthNames[date.getMonth()] + " " +
+        date.getDate() + ", " + date.getFullYear() + " " +
+        hours + ":" + paddedMinutes + period;
 }
