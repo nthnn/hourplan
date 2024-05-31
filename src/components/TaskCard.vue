913bd109433ff8fdd020fa247aa1ddaaefd4fe55
Nathanne <nathanneisip@gmail.com>
2024-06-01 02:33:18 +0800

    Checkbox status on TaskCard component.

--- src/components/TaskCard.vue
+++ src/components/TaskCard.vue
@@ -1,7 +1,13 @@
 <script lang="ts">
+import $ from "jquery";
 import { toJSDate, toShortDate } from '@/assets/scripts/time';
 
 export default {
+    data() {
+        return {
+            taskId: "task-" + Math.round(Math.random() * 100000)
+        }
+    },
     props: {
         title: {type: String},
         desc: {type: String},
@@ -17,13 +23,18 @@ export default {
         shortEndDate(): string {
             return toShortDate(toJSDate(this.endDate as number));
         }
+    },
+    methods: {
+        onMarked(): void {
+            alert($("#" + this.taskId).is(":checked"));
+        }
     }
 };
 </script>
 
 <template>
     <div class="w-100 rounded border-none shadow p-2" :style="{ backgroundColor: color}">
-        <div class="row">
+        <div class="row p-2">
             <div class="col-4 col-lg-3">
                 <div v-if="emotion === 'happy'">
                     <img src="@/assets/images/cat-happy.png" class="w-100 p-2" />
@@ -46,10 +57,30 @@ export default {
                 </div>
             </div>
 
-            <div class="col-2 col-2"></div>
+            <div class="col-2">
+                <div class="d-flex align-items-center h-100 w-100 m-0 p-0">
+                    <input type="checkbox"
+                        class="form-check-input" value=""
+                        :id=taskId @click="onMarked" />
+                </div>
+            </div>
         </div>
 
         <small class="text-lato px-2 mb-2 mobile-only text-muted">{{ shortStartDate }} &mdash; {{ shortEndDate }}</small>
     </div>
     <br/>
 </template>
+
+<style scoped>
+input[type="checkbox"] {
+    width: 28px !important;
+    height: 28px !important;
+    outline: none !important;
+    border: none !important;
+    margin-top: -12px;
+}
+
+input[type="checkbox"]:checked {
+    background-color: #1a172a !important;
+}
+</style>
