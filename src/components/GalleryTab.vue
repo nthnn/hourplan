50213504ee830ef0e7f665bf4a8fdf82d4215c16
Nathanne <nathanneisip@gmail.com>
2024-06-04 23:57:07 +0800

    Initial GalleryTab component for ProfilePage view.

---
+++ src/components/GalleryTab.vue
@@ -0,0 +1,13 @@
+<script lang="ts">
+export default {
+    props: {
+        show: {type: Boolean}
+    }
+};
+</script>
+
+<template>
+    <div v-if="show" class="container">
+        <h1>Gallery</h1>
+    </div>
+</template>
