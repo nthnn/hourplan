fb19575979a259beb7fbd5ab41c30b206fe0d06f
Nathanne <nathanneisip@gmail.com>
2024-06-04 23:58:37 +0800

    Initial AchievementsTab component.

---
+++ src/components/AchievementsTab.vue
@@ -0,0 +1,13 @@
+<script lang="ts">
+export default {
+    props: {
+        show: {type: Boolean}
+    }
+};
+</script>
+
+<template>
+    <div v-if="show" class="container">
+        <h1>Achievements</h1>
+    </div>
+</template>
