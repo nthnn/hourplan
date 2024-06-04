e3544bb29d632caeb644c723c6305cd300268901
Nathanne <nathanneisip@gmail.com>
2024-06-04 23:58:12 +0800

    Initial FinishedTaskTab component.

---
+++ src/components/FinishedTaskTab.vue
@@ -0,0 +1,13 @@
+<script lang="ts">
+export default {
+    props: {
+        show: {type: Boolean}
+    }
+};
+</script>
+
+<template>
+    <div v-if="show" class="container">
+        <h1>Finished Tasks</h1>
+    </div>
+</template>
