c9f6b73750e703f67469c7fe6d8ffcbce464cd9b
Nathanne <nathanneisip@gmail.com>
2024-06-07 00:13:52 +0800

    Fetching task with retrieval algorithm for repeating tasks.

--- src/components/NewTaskModal.vue
+++ src/components/NewTaskModal.vue
@@ -178,8 +178,8 @@ onMounted(()=> {
                     <select id="task-repeat" name="task-repeat" class="form-control text-lato" required>
                         <option class="text-lato" value="0">Does not repeat</option>
                         <option class="text-lato" value="1">Every Day</option>
-                        <option class="text-lato" value="3">Every Month</option>
-                        <option class="text-lato" value="4">Every Year</option>
+                        <option class="text-lato" value="2">Every Month</option>
+                        <option class="text-lato" value="3">Every Year</option>
                     </select>
 
                     <label for="task-end" class="form-label text-lato mt-2">Ends</label>
