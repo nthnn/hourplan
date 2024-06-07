6b94c89132e008657b7c6f27256ee5a679a49ae6
Nathanne <nathanneisip@gmail.com>
2024-06-07 23:12:03 +0800

    Improved time utility function in backend libraries.

--- backend/lib/time.php
+++ backend/lib/time.php
@@ -1,7 +1,21 @@
 <?php
 
+date_default_timezone_set("Asia/Manila");
+
 function getTimeForTodayBasedOn($timestamp) {
     return strtotime(date("Y-m-d").' '.date("H:i:s", $timestamp));
+}
+
+function getDateTimestampBasedOn($timestamp) {
+    return strtotime(
+        sprintf("%04d-%02d-%02d %02d:%02d:%02d", 
+            date("Y"),
+            date("m"),
+            date("d", $timestamp),
+            date("H", $timestamp),
+            date("i", $timestamp),
+            date("s", $timestamp))
+    );
 }
 
 ?>
