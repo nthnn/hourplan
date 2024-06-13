e2ce1f037490c968447978611668a2927cb3b397
Nathanne Isip <nathanneisip@gmail.com>
2024-06-14 03:15:05 +0800

    App usage for Vuetify on main.ts

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,23 @@ import 'v-calendar/style.css';
 import "../node_modules/bootswatch/dist/lumen/bootstrap.min.css";
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
 
+import "vuetify/styles";
+import { createVuetify, type VuetifyOptions } from 'vuetify';
+import * as VuetifyComponents from "vuetify/components";
+import * as VuetifyDirectives from "vuetify/directives";
+
+interface VuetifyComponentsType extends VuetifyOptions {
+    VuetifyComponents?: any;
+    VuetifyDirectives?: any;
+}
+
 const app: App<Element> = createApp(Application);
+const vuetify = createVuetify({
+    VuetifyComponents,
+    VuetifyDirectives,
+} as VuetifyComponentsType);
+
 app.use(router);
+app.use(vuetify);
 app.use(VCalendar, {});
 app.mount('#app');
