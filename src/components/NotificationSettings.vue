bf3f3be49d830648ef5a31f7ae506b689e0b84e8
Nathanne Isip <nathanneisip@gmail.com>
2024-06-15 23:14:44 +0800

    Added message warning on notification settings.

--- src/components/NotificationSettings.vue
+++ src/components/NotificationSettings.vue
@@ -4,4 +4,9 @@ import { detectMobile } from '@/assets/scripts/mobile_detect';
 
 <template>
     <h1 v-if="!detectMobile()">Notifications</h1>
+    <hr class="desktop-only"/>
+
+    <div class="bg-warning rounded p-2 text-lato">
+        Notification setting is only for mobile app version of HourPlan.
+    </div>
 </template>
