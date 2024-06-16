86b34ca6b6dec3cd9bbefb242a1bc93ccf997b82
Nathanne Isip <nathanneisip@gmail.com>
2024-06-16 16:52:59 +0800

    Function rendition of FnishedTaskTab component for ProfilePage.

--- src/components/FinishedTaskTab.vue
+++ src/components/FinishedTaskTab.vue
@@ -1,5 +1,17 @@
+<script setup lang="ts">
+import TaskList from "./TaskList.vue";
+</script>
+
 <script lang="ts">
+import env from '@/assets/scripts/config';
+
 export default {
+    data() {
+        return {
+            currentTheme: localStorage.getItem("theme") as string,
+            publicPath: env.host.split("/")[0] + "/"
+        }
+    },
     props: {
         show: {type: Boolean}
     }
@@ -9,5 +21,15 @@ export default {
 <template>
     <div v-if="show" class="container">
         <h1>Finished Tasks</h1>
+        <hr/>
+
+        <div id="finished-tasks">
+            <TaskList apiAction="all_task" :finished="true" :large="true" />
+        </div>
+
+        <div id="no-finished-tasks" align="center">
+            <img :src="`${publicPath}images/cat-happy-${currentTheme}.png`" width="120" />
+            <p>No finished task yet.</p>
+        </div>
     </div>
 </template>
